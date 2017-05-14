f1e8cd61b14a5289a1a035d87e45cb761c3a9238
Mischa Holz <me@mhlz.de>
2017-05-14 10:20:16 +0200

    :tada:

---
+++ src/main/kotlin/de/mhlz/kotlint/rules/MaximumLineLengthRule.kt
@@ -0,0 +1,44 @@
+package de.mhlz.kotlint.rules
+
+import de.mhlz.kotlint.Config
+import de.mhlz.kotlint.Emitter
+import de.mhlz.kotlint.ProblemLevel
+import de.mhlz.kotlint.Rule
+import org.jetbrains.kotlin.KtNodeTypes
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.FileElement
+import org.jetbrains.kotlin.com.intellij.psi.tree.TokenSet
+
+/**
+ * @author Mischa Holz
+ */
+class MaximumLineLengthRule : Rule("maximum-line-length") {
+
+    private val maxLength: Int = Config["maximum-line-length"]["length"] as Int
+
+    override fun lint(node: PsiElement, emit: Emitter) {
+        if (node is FileElement) {
+            val text = (node as FileElement).text
+
+            val lines = text.split("\n")
+
+            val importStatements = node.getChildren(TokenSet.create(KtNodeTypes.IMPORT_LIST))
+            val importTextRange = importStatements.firstOrNull()?.textRange
+
+            lines.forEachIndexed { i, line ->
+                val offset = lines.subList(0, i).map { it.length + 1 }.sum()
+
+                val partOfImportList = importTextRange?.contains(offset) ?: false
+
+                if (line.length < maxLength || partOfImportList) return@forEachIndexed
+
+                emit(
+                        offset,
+                        ProblemLevel.error,
+                        "This line is too long. Shorten it to less than $maxLength chars",
+                        null
+                )
+            }
+        }
+    }
+}
